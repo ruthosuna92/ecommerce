3b1e110e63e6f08bcea3f065d68edeb42136a6cf
alejandra <alejandra@emotecare.com>
2024-10-15 17:46:01 +0100

    intalling

---
+++ my-app/src/routes/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { cartCount } from '$lib/stores/cartStore.js';
+
+    export let id
+    export let title
+    export let description
+
+</script>
+
+
+<div class="w-40 h-60 border border-blue-500">
+    <h1 class="text-2xl">{id}</h1>
+    <h1 class="text-2xl">{title}</h1>
+    <p class="text-lg">{description}</p>
+    <button on:click="{() => {
+        //aquí verificamos si el carrito está vacío y le colocamos directamente el numero 1
+        if(!$cartCount){
+            cartCount.set(1)
+            //si el carrito no está vacío, le sumamos 1
+        } else {
+            cartCount.update((n) => n + 1);
+        }
+         }}"  class="bg-blue-500 text-white">Click me</button>
+</div>
---
+++ my-app/src/routes/ecommerce/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Card from "../Card.svelte";
+
+    let cards = [
+        { id: 1, title: "Card 1", description: "This is card 1" },
+        { id: 2, title: "Card 2", description: "This is card 2" },
+        { id: 3, title: "Card 3", description: "This is card 3" },
+    ];
+
+</script>
+<header>
+    <h1 class="bg-red-500">ecommerce/+page</h1>
+</header>
+
+<div class="flex flex-row gap-6 p-8">
+    {#each cards as card}
+        <Card id={card.id} title={card.title} description={card.description} />
+    {/each}
+</div>
